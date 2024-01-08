2bd0e38a3ad4f9d228907382e82611a794b6c3e4
Mahmoud Gamal <Mahmoudgamalawad10@gmail.com>
2024-01-08 12:16:30 +0200

    Markup and style

---
+++ script.js
@@ -0,0 +1,18 @@
+// let getLocation = () => {
+//   navigator.geolocation.getCurrentPosition((position) => {
+//     console.log(position);
+//   });
+// };
+
+// getLocation();
+
+// let getWeather = async () => {
+//   let key = "0150b5277f0d45438b475635240701";
+//   let result = await fetch(
+//     `https://api.weatherapi.com/v1/forecast.json?key=${key}&q=London&days=7`
+//   );
+//   let data = await result.json();
+//   console.log(data);
+// };
+
+// getWeather();
